aa05af6efca49d16ffc52013d5803803bd222388
Evan <you@example.com>
2023-10-31 18:05:23 -0400

    added search bar

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,14 +1,32 @@
-import React from "react";
+
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import SearchAppBar from "./SearchAppBar";
+import React, { useState, useEffect } from "react";
 
 function RecipeDetails(props) {
   const { data } = props;
   const { label } = useParams();
   console.log("data:", data);
+  const [searchQuery, setSearchQuery] = useState(""); // set state for search query and filtered recipes
+  const [filteredRecipes, setFilteredRecipes] = useState(data);
 
-  // Find the selected recipe based on the label
-  const selectedRecipe = data.find((recipe) => recipe.recipe.label === label);
+  useEffect(() => {
+    setFilteredRecipes(data); // starts filtered recipes with data
+  }, [data]);
+
+  const handleSearch = (query) => {
+    // handle the searchquery
+    setSearchQuery(query);
+    const filtered = data.filter(
+      (recipe) =>
+        recipe.recipe.label.toLowerCase().includes(query.toLowerCase()) //make it where it can do querys even with capital letters
+    );
+    setFilteredRecipes(filtered);
+  };
+
+  
+  const selectedRecipe = data.find((recipe) => recipe.recipe.label === label); // .find the selected recipe based on the label
 
   if (!selectedRecipe) {
     return <div>Recipe not found</div>;
@@ -16,6 +34,7 @@ function RecipeDetails(props) {
 
   return (
     <div>
+      <SearchAppBar onSearch={handleSearch} recipes={data} />
       <h1>Recipe Details for: {label}</h1>
       <p>
         <h2>Calories:</h2> {selectedRecipe.recipe.calories} kcal
@@ -59,7 +78,7 @@ function RecipeDetails(props) {
         </h2>
       </p>
 
-      <Link to={".."}>Back</Link>
+
     </div>
   );
 }
