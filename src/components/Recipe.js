0c29730a622b2bcee1c4ab3d05369b0669717e2c
Evan <you@example.com>
2023-10-30 20:00:56 -0400

    added card

---
+++ src/components/Recipe.js
@@ -0,0 +1,67 @@
+//** Import Statements
+import React, { useState, useEffect } from "react"
+import Backdrop from '@mui/material/Backdrop'
+import CircularProgress from '@mui/material/CircularProgress'
+import Card from './Card'
+import Grid from '@mui/material/Grid';
+
+
+//** Setup (define helper functions and variables here)
+
+function Recipe(props) {
+  //** Destructure Props
+  const {
+    data,
+    error
+  } = props
+
+  if (error) {
+    return <div>Error: {error}</div>
+  }
+  
+
+  if (data.length === 0) {
+    return (
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={true}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    );
+  }
+
+  //** Return JSX
+  return (
+    <Grid
+    container
+    direction="row"
+    justifyContent="space-around"
+    alignItems="flex-start"
+    rowSpacing={4}
+    columnSpacing={1}
+    >
+
+      {data.map((data, index) => {
+        console.log('title: ', data.hits[2])
+        
+        return (
+          <Grid item xs={12} sm={9} md={6} lg={3} xl={1}>
+          <Card
+          title={data.hits[index].recipe.label}
+          image={data.hits[index].recipe.images.REGULAR}
+          calories={data.hits[index].recipe.calories}
+          servingCount={data.hits[index].recipe.yield}
+          />
+          </Grid>
+          
+
+          
+        )
+      })}
+
+    </Grid>
+  )
+}
+
+export default Recipe;
