98c9c50faddb84cbbbfc679cf6da9039443c7b4c
Evan <you@example.com>
2023-11-01 12:37:41 -0400

    Added Share button

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -7,21 +7,23 @@ import SearchAppBar from "./SearchAppBar";
 import Sidebar from "./Sidebar";
 
 function Recipe(props) {
-  const { data, error } = props;//props
+  const { data, error } = props; //props
 
-  const [searchQuery, setSearchQuery] = useState("");//searchquery for search bar
+  const [searchQuery, setSearchQuery] = useState(""); //searchquery for search bar
   const [selectedFilters, setSelectedFilters] = useState([]);
-  const [noAllergens, setNoAllergens] = useState(false);//usestate for no allergens
+  const [noAllergens, setNoAllergens] = useState(false); //usestate for no allergens
 
   useEffect(() => {
     setNoAllergens(false); // if no allergen checkboxes are checked, will only rely on the searchquery on app bar
   }, [selectedFilters]);
 
-  const handleSearch = (query) => {//handles search event
+  const handleSearch = (query) => {
+    //handles search event
     setSearchQuery(query);
   };
 
-  const handleFilterChange = (filters) => {//handles filtered recipes with checkboxes
+  const handleFilterChange = (filters) => {
+    //handles filtered recipes with checkboxes
     setSelectedFilters(filters);
   };
 
@@ -36,27 +38,30 @@ function Recipe(props) {
         open={true}
       >
         <CircularProgress color="inherit" />
-      </Backdrop>//this is useless
+      </Backdrop> //this is useless
     );
   }
 
-  
   const filteredRecipes = data.filter((recipe) => {
     const searchMatch =
-      recipe.recipe.label.toLowerCase().includes(searchQuery.toLowerCase()) ||//search by recipe name
-      recipe.recipe.ingredientLines.some((ingredient) =>//OR search by ingredient
-        ingredient.toLowerCase().includes(searchQuery.toLowerCase())//inputs will become lowercase in searchquery
+      recipe.recipe.label.toLowerCase().includes(searchQuery.toLowerCase()) || //search by recipe name
+      recipe.recipe.ingredientLines.some(
+        (
+          ingredient //OR search by ingredient
+        ) => ingredient.toLowerCase().includes(searchQuery.toLowerCase()) //inputs will become lowercase in searchquery
       );
 
     const filtersMatch =
       noAllergens || // default to no allergens with no checkbox checked
-      (selectedFilters.length === 0//if there are no checkboxes checked, it will result in true
+      (selectedFilters.length === 0 //if there are no checkboxes checked, it will result in true
         ? true
-        : selectedFilters.every((filter) =>//if there are checkboxes being checked, it would check if the recipes are satisfying the health labels and search querys
-            recipe.recipe.healthLabels.includes(filter)
+        : selectedFilters.every(
+            (
+              filter //if there are checkboxes being checked, it would check if the recipes are satisfying the health labels and search querys
+            ) => recipe.recipe.healthLabels.includes(filter)
           ));
 
-    return searchMatch && filtersMatch;//needs to match both searchquery and fiilters to get correct list of recipes
+    return searchMatch && filtersMatch; //needs to match both searchquery and fiilters to get correct list of recipes
   });
 
   return (
@@ -69,9 +74,11 @@ function Recipe(props) {
           <br></br>
           <Sidebar
             onFilterChange={handleFilterChange} //handlefiltter change from sidebnar component
-            healthLabels={Array.from(new Set(data.map((item) => item.recipe.healthLabels).flat()))}//got the health label/allergens from data, make it into one array with.flat, Set removes duplicated healthlabels from the array. array.from makes it an array again(copied from stackoverflow, dont hate me)
-            noAllergens={noAllergens}//default state/no checkboxes are checked
-            setNoAllergens={setNoAllergens}//set state to default
+            healthLabels={Array.from(
+              new Set(data.map((item) => item.recipe.healthLabels).flat())
+            )} //got the health label/allergens from data, make it into one array with.flat, Set removes duplicated healthlabels from the array. array.from makes it an array again(copied from stackoverflow, dont hate me)
+            noAllergens={noAllergens} //default state/no checkboxes are checked
+            setNoAllergens={setNoAllergens} //set state to default
           />
         </Grid>
         <Grid item xs={12}>
